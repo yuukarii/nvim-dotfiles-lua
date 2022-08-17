467f540db9c14e877414773d29eff224d00af232
DNomadVN <vovandoan4815@gmail.com>
2022-08-17 17:02:08 +0700

    Use separate file to config map key for coc-nvim plugin

---
+++ lua/plugins_setup/coc-setup.lua
@@ -0,0 +1,40 @@
+-- Mapping with coc plugin
+-- Make <CR> to accept selected completion item
+--
+-- Use `[g` and `]g` to navigate diagnostics
+-- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
+--
+-- Use gd, gy, gi, gr to go to code navigation
+--
+-- Use K to show documentation in preview window.
+--
+-- Highlight the symbol and its references when holding the cursor.
+--
+-- Symbol renaming by \rn
+--
+vim.cmd([[
+inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+        \: "\<CR>\<c-r>=coc#on_enter()\<CR>"
+
+nmap <silent> [g <Plug>(coc-diagnostic-prev)
+nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+nmap <silent> gd <Plug>(coc-definition)
+nmap <silent> gy <Plug>(coc-type-definition)
+nmap <silent> gi <Plug>(coc-implementation)
+nmap <silent> gr <Plug>(coc-references)
+
+nnoremap <silent> K :call ShowDocumentation()<CR>
+
+function! ShowDocumentation()
+  if CocAction('hasProvider', 'hover')
+    call CocActionAsync('doHover')
+  else
+    call feedkeys('K', 'in')
+  endif
+endfunction
+
+autocmd CursorHold * silent call CocActionAsync('highlight')
+
+nmap <leader>rn <Plug>(coc-rename)
+]])
