0eb613ea6fa88ab91b328ce3d2a857138f77082f
DNomadVN <vovandoan4815@gmail.com>
2022-08-21 18:59:35 +0700

    Use theta theme for alpha-nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,7 +15,7 @@ return require('packer').startup({function(use)
   use {
     'goolord/alpha-nvim',
     config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        require'alpha'.setup(require'alpha.themes.theta'.config)
     end
   }
 
