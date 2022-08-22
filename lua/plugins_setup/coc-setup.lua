ee51784e37248a2a91fcd3ac28413862382b3121
DNomadVN <vovandoan4815@gmail.com>
2022-08-22 15:45:03 +0700

    Add some key bindings for Coc

--- lua/plugins_setup/coc-setup.lua
+++ lua/plugins_setup/coc-setup.lua
@@ -69,4 +69,14 @@ nnoremap <silent><nowait> <space>o  :<C-u>CocList outline<cr>
 
 " Restart Coc
 nnoremap <silent> <F5> :<C-u>CocRestart<cr>
+
+" Remap <C-f> and <C-b> for scroll float windows/popups.
+if has('nvim-0.4.0') || has('patch-8.2.0750')
+  nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+  nnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+  inoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
+  inoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
+  vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+  vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+endif
 ]])
