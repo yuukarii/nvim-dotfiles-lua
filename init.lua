eb4776093e7a302d8c21289ee3514dbc77154b6b
DNomadVN <vovandoan4815@gmail.com>
2022-08-23 15:49:10 +0700

    Add condition to choose what lua setup to load

--- init.lua
+++ init.lua
@@ -5,4 +5,10 @@ local ok, _ = pcall(require, 'plugins_setup.nvim-tree')
 local ok, _ = pcall(require, 'plugins_setup.lualine')
 local ok, _ = pcall(require, 'plugins_setup.treesitter')
 local ok, _ = pcall(require, 'plugins_setup.coc-setup')
-local ok, _ = pcall(require, 'plugins_setup.barbar-setup')
+if (vim.loop.os_uname().sysname == "Darwin") then
+  print("You are using MacOS. Trying to load setup for MacOS...")
+  local ok, _ = pcall(require, 'plugins_setup.barbar-setup-mac')
+else 
+  print("You aren't using MacOS. Trying to load normal setup...")
+  local ok, _ = pcall(require, 'plugins_setup.barbar-setup')
+end
