2775e907454949b4e9dfb0dedabc5e1477fde8ca
Nomad <nomad@Miyuki.localdomain>
2022-09-14 21:50:17 +0700

    Remove local ok, _

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 require('plugins')
 
-local ok, _ = pcall(require, 'setup')
-local ok, _ = pcall(require, 'plugins_setup.nvim-tree')
-local ok, _ = pcall(require, 'plugins_setup.lualine')
-local ok, _ = pcall(require, 'plugins_setup.treesitter')
-local ok, _ = pcall(require, 'plugins_setup.coc-setup')
+pcall(require, 'setup')
+pcall(require, 'plugins_setup.nvim-tree')
+pcall(require, 'plugins_setup.lualine')
+pcall(require, 'plugins_setup.treesitter')
+pcall(require, 'plugins_setup.coc-setup')
 
 if (vim.loop.os_uname().sysname == "Darwin") then
   print("You are using MacOS. Trying to load setup for MacOS...")
   require('mapping-mac')
-  local ok, _ = pcall(require, 'plugins_setup.barbar-setup-mac')
-else 
+  pcall(require, 'plugins_setup.barbar-setup-mac')
+else
   print("You aren't using MacOS. Trying to load normal setup...")
   require('mapping')
-  local ok, _ = pcall(require, 'plugins_setup.barbar-setup')
+  pcall(require, 'plugins_setup.barbar-setup')
 end
--- lua/plugins_setup/treesitter.lua
+++ lua/plugins_setup/treesitter.lua
@@ -1,7 +1,4 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "java", "python", "perl" },
-
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
